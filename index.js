d45f0737ebdc14add08febc1642642c1ba88e756
Aayush <aayush9152@gmail.com>
2020-07-28 17:00:15 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const otp = (digits) => {
+	let otp = '';
+	for (let i = 1; i <= digits; i++) {
+		if (i % 2 === 0) {
+			otp = otp + String(Math.floor(Math.random() * 11));
+		} else {
+			let num = Math.floor(Math.random() * (122 - 97)) + 97;
+			otp = otp + String.fromCharCode(num);
+		}
+	}
+	return otp;
+};
+
+module.exports = {
+	otp,
+};
